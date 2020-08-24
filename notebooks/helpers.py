3835175718da706969ef0881874418f725de0370
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-08-24 14:57:41 -0600

    Add updated notebooks

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -3,9 +3,14 @@ import random
 import shutil
 from functools import reduce
 from operator import mul
+from pathlib import Path
 
+import numpy as np
 import xarray as xr
 from distributed.utils import format_bytes
+from tqdm.auto import tqdm
+
+random.seed(42)
 
 
 def print_ds_info(ds, var):
@@ -44,14 +49,20 @@ def zarr_store(
 
 def save_data(ds, store):
     try:
-        ds.to_zarr(store, consolidated=True)
+        ds.to_zarr(store, consolidated=True, mode='w')
         del ds
     except Exception as e:
         print(f'Failed to write {store}: {e}')
 
 
 def process_variables(
-    col, variables, component, stream, experiment, verbose=False
+    col,
+    variables,
+    component,
+    stream,
+    experiment,
+    date_range=None,
+    verbose=False,
 ):
     query = dict(
         component=component,
@@ -59,11 +70,19 @@ def process_variables(
         variable=variables,
         experiment=experiment,
     )
+    if date_range:
+        query['date_range'] = date_range
     subset = col.search(**query)
     if verbose:
         print(
             subset.unique(
-                columns=['variable', 'component', 'stream', 'experiment']
+                columns=[
+                    'variable',
+                    'component',
+                    'stream',
+                    'experiment',
+                    'date_range',
+                ]
             )
         )
     return subset, query
@@ -92,29 +111,74 @@ def enforce_chunking(datasets, chunks, field_separator):
 
 
 def get_grid_vars(ds, variables):
-    vars_to_drop = [vname for vname in ds.data_vars if vname not in variables]
     coord_vars = [
         vname
         for vname in ds.data_vars
         if 'time' not in ds[vname].dims or 'bound' in vname
     ]
     ds_fixed = ds.set_coords(coord_vars)
-    data_vars_dims = []
-    for data_var in ds_fixed.data_vars:
-        data_vars_dims.extend(list(ds_fixed[data_var].dims))
-    coords_to_drop = [
-        coord for coord in ds_fixed.coords if coord not in data_vars_dims
-    ]
-    grid_vars = list(
-        set(vars_to_drop + coords_to_drop) - set(['time', 'time_bound'])
-    )
-
+    grid_vars = list(ds_fixed.drop_dims('time').coords.keys())
     return grid_vars
 
 
-def create_grid_dataset(sample_file, variables):
-    ds = xr.open_dataset(sample_file, chunks={}, decode_times=False)
+def create_grid_dataset(sample_file, variables, vars_to_drop=None):
+    ds = xr.open_dataset(sample_file, decode_times=False)
     grid_vars = get_grid_vars(ds, variables)
-    grid = ds.set_coords(grid_vars)[grid_vars].drop_dims('time')
+    grid = ds.set_coords(grid_vars)[grid_vars]
+    if vars_to_drop:
+        to_drop = {vname for vname in vars_to_drop if vname in grid.coords}
+        grid = grid.drop(list(to_drop))
     grid.attrs = {}
     return grid
+
+
+def fix_time(
+    ds,
+    start,
+    end,
+    freq,
+    time_bounds_dim,
+    calendar='noleap',
+    generate_bounds=True,
+    instantaneous=False,
+):
+    ds = ds.sortby('time').copy()
+    attrs = ds.time.attrs
+    encoding = ds.time.encoding
+    bounds_name = ds.time.attrs['bounds']
+    ds[bounds_name].load()
+    if generate_bounds:
+        times = xr.cftime_range(
+            start=start, end=end, freq=freq, calendar=calendar
+        )
+        bounds = np.vstack([times[:-1], times[1:]]).T
+        ds[bounds_name].data = bounds
+
+    if instantaneous:
+        ds = ds.assign_coords(time=ds[bounds_name].min(time_bounds_dim))
+    else:
+        ds = ds.assign_coords(time=ds[bounds_name].mean(time_bounds_dim))
+    ds.time.attrs = attrs
+    ds.time.encoding = encoding
+    ds = ds.set_coords([bounds_name])
+    return ds
+
+
+def inspect_written_stores(dirout, random_sample_size=None):
+    faulty_stores = []
+    p = Path(dirout)
+    stores = list(p.rglob('*/*.zarr'))
+    print(f'Total number of discovered zarr stores: {len(stores)}')
+    if random_sample_size:
+        stores = random.choices(stores, k=random_sample_size)
+    for store in tqdm(stores):
+        try:
+            ds = xr.open_zarr(store.as_posix(), consolidated=True)
+            print('\n')
+            print(f'store path: {store}')
+            print(f'Time range: {ds.time.min().data} --> {ds.time.max().data}')
+        except Exception as e:
+            faulty_stores.append(store)
+            print(e)
+    if faulty_stores:
+        print(f'Faulty stores: {faulty_stores}')
